df7160d1d82312710503b408d6c5567ebeb42d70
fahmidaNimra173 <fahmidanimra@gmail.com>
2026-02-05 17:12:45 +0600

    created all the routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'index'])->name('home');
+Route::get('/todos', [TodoController::class, 'postTodo']);
+Route::post('/todos/update/{id}', [TodoController::class, 'updateTodo']);
+Route::get('/todos/status/{id}', [TodoController::class, 'updateStatus']);
+Route::post('/todos/edit/{id}', [TodoController::class, 'edit']);
+Route::get('/todos/delete/{id}', [TodoController::class, 'delete']);
