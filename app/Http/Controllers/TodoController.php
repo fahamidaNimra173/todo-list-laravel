7fa40f28cbb76bce97741e15b7da2dc161915403
fahmidaNimra173 <fahmidanimra@gmail.com>
2026-02-05 17:15:05 +0600

    implemented postTodo function at TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -16,6 +16,30 @@ class TodoController extends Controller
         return view('home', compact('todos'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function postTodo(Request  $request)
+    {
+        $validate = $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'image' => 'nullable|image|mimes:jpg,png,jpeg'
+
+        ]);
+        $image = null;
+        if ($request->hasFile('image')) {
+            $image = time() . '_' . $request->image->getClientOriginalName();
+            $request->image->move(public_path('images'), $image);
+        }
+        $todo = new Todo;
+        $todo->name = $request->name;
+        $todo->price = $request->price;
+        $todo->image = $image;
+        $todo->save();
+        return redirect()->route('home')->with('success', 'Todo created successfully');
+    }
+
 
     /**
      * Show the form for editing the specified resource.
